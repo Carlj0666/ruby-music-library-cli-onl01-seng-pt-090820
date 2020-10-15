7495da6126f9f6e55837ac77a1c2a7deab536746
Carl J <carlj1881@yahoo.com>
2020-10-15 19:17:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,55 +1,55 @@
-require "pry"
+# require "pry"
 
-class Artist
+# class Artist
   
-  attr_accessor :name
+#   attr_accessor :name
   
-  @@all = []
+#   @@all = []
   
-  def initialize(name)
-    @name = name
-    @@all << self
-    @songs = []
-  end
-
-
-  def self.all
-    @@all
-  end
-  
-  
-  def save
-    self.class.all << self
-  end
-  
-  
-  def self.destroy_all
-    self.all.clear
-  end
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#     @songs = []
+#   end
 
 
-  def self.create(name)
-    artist = Artist.new(name)
-    artist.save
-    artist
-  end
+#   def self.all
+#     @@all
+#   end
   
   
-   def add_song(song)
+#   def save
+#     self.class.all << self
+#   end
+  
+  
+#   def self.destroy_all
+#     self.all.clear
+#   end
+
+
+#   def self.create(name)
+#     artist = Artist.new(name)
+#     artist.save
+#     artist
+#   end
+  
+  
+#   def add_song(song)
     
-    if song.artist == nil
-      @songs << song
-    song.artist = self
-    end
-  end
+#     if song.artist == nil
+#       @songs << song
+#     song.artist = self
+#     end
+#   end
   
   
-  def songs
-    Song.all.select {|song| song.artist == self}
-    @songs
-  end
+#   def songs
+#     Song.all.select {|song| song.artist == self}
+#     @songs
+#   end
   
-  def genres
+#   def genres
     
-  end  
-end
+#   end  
+# end
