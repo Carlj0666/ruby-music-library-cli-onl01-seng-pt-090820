6c0c82d1ec21efb205e20fd25def31c51eb738f9
Carl J <carlj1881@yahoo.com>
2020-10-15 19:18:09 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,26 @@
+class Genre
+  
+    attr_accessor :name
+    attr_reader :songs
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    
+  end
+
+
+
+
+
+
 # require "pry"
 
 # class Genre
