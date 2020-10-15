d63434988fd87083bb0ca1b3c969fd5f13fc0318
Carl J <carlj1881@yahoo.com>
2020-10-15 19:19:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,26 @@
+class Song
+    attr_accessor :name
+    attr_reader :artist, :genre
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+    end
+
+  
+    def self.all
+      @@all
+    end
+  
+    
+  end
+
+
+
+
+
+
 
 
 # require "pry"
